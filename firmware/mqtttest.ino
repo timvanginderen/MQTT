11f6a7c858b80aec6cd0189e17650c1146767bf0
Tim Van Ginderen <timvanginderen@gmail.com>
2015-10-30 04:28:37 +0100

    Add my mqtttest.ino file to firmware dir

---
+++ firmware/mqtttest.ino
@@ -0,0 +1,61 @@
+#include "MQTT.h"
+
+void callback(char* topic, byte* payload, unsigned int length);
+
+/**
+ * if want to use IP address,
+ * byte server[] = { XXX,XXX,XXX,XXX };
+ * MQTT client(server, 1883, callback);
+ * want to use domain name,
+ * MQTT client("www.sample.com", 1883, callback);
+ **/
+byte server[] = { 192,168,1,20 };
+MQTT client(server, 1883, callback);
+
+byte bytebuffer[30];
+int analogPin = A0;
+
+
+// recieve message
+void callback(char* topic, byte* payload, unsigned int length) {
+    char p[length + 1];
+    memcpy(p, payload, length);
+    p[length] = NULL;
+    String message(p);
+
+    if (message.equals("RED"))
+        RGB.color(255, 0, 0);
+    else if (message.equals("GREEN"))
+        RGB.color(0, 255, 0);
+    else if (message.equals("BLUE"))
+        RGB.color(0, 0, 255);
+    else
+        RGB.color(255, 255, 255);
+    delay(1000);
+}
+
+
+void setup() {
+    RGB.control(true);
+
+    // connect to the server
+    client.connect("sparkclient");
+
+    // publish/subscribe
+    if (client.isConnected()) {
+        client.subscribe("amoeder");
+        client.publish("amoeder","hello world");
+        
+        int sensorValue = analogRead(analogPin);
+
+        String message = String(sensorValue);
+        message.getBytes(bytebuffer, 20);
+
+        client.publish("amoeder", bytebuffer, 20);
+    }
+}
+
+void loop() {
+    if (client.isConnected())
+        client.loop();
+}
